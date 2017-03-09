d69c1d0e6a617f0232baff62493ca59fc6700b29
maggie97 <maggielee97@icloud.com>
2017-03-09 13:00:59 -0600

    js quiery  y eventos

---
+++ holajquery.js
@@ -0,0 +1,13 @@
+$(function()
+{
+    $nuevo = $('<P> parrafo nuevo </p>')
+    $('#boton1')
+    $('h2').after($nuevo)
+    $(selector).after($nuevo)
+    e.preventDefault();
+    e.
+   
+        
+    });
+     $(selector).on(events, function () {
+})
